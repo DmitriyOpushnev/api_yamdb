6bd13cdb3a0b980737789ee71716d5e6feb27bab
Ruslan Atarov <ruslanatarov@gmail.com>
2023-02-05 15:53:17 +0300

    bugfix: correct score and filter for titles

---
+++ api_yamdb/api/filters.py
@@ -0,0 +1,11 @@
+from django_filters import rest_framework as filters
+from reviews.models import Title
+
+
+class TitleFilter(filters.FilterSet):
+    category = filters.CharFilter(field_name='category__slug')
+    genre = filters.CharFilter(field_name='genre__slug')
+
+    class Meta:
+        model = Title
+        fields = ('category', 'genre', 'name', 'year')
